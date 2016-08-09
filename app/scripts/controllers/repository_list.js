fef3061c963a28d4dbdf91f0fa3e6d6e90b71814
Christoph Witzany <christoph@web.crofting.com>
2016-08-09 16:26:03 +0200

    Cleanup

--- app/scripts/controllers/repository_list.js
+++ app/scripts/controllers/repository_list.js
@@ -13,7 +13,4 @@ angular.module('marinaFrontendApp')
     function ($scope, repositoryList, $stateParams) {
       $scope.namespace = $stateParams.owner || 'All';
       $scope.repositories = repositoryList;
-      console.log($scope.repositories);
-
-
   }]);
