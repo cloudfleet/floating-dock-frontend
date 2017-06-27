b5775477eb32abf8f0edea72a8c4c6abccdc1d01
Christoph Witzany <christoph@web.crofting.com>
2017-06-27 19:06:18 +0200

    add api key UI to user profile

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -9,9 +9,33 @@
  */
 angular.module('marinaFrontendApp')
   .controller('ProfileCtrl',
-  [ '$scope', '$stateParams','marinaApi', 'profile_user',
-    function ($scope, $stateParams, marinaApi, profile_user) {
+  [ '$scope', '$stateParams', '$location', 'marinaApi', 'profile_user',
+    function ($scope, $stateParams, $location, marinaApi, profile_user) {
       $scope.profile_user = profile_user;
-      console.log($scope.profile_user);
+
+
+      $scope.web_hook_base_url =
+          $location.protocol() +
+          '://'+ $location.host() +
+          '/api/v1/api/v1/github/pushes';
+
+      $scope.showApiKey = function() {
+        if(!profile_user.api_key)
+        {
+          marinaApi.User.generateApiKey({id: profile_user.id}).$promise.then(function() {
+            profile_user.$get().then(function(){
+              $scope.api_key = profile_user.api_key;
+            });
+          });
+        }
+        else {
+          $scope.api_key = profile_user.api_key;
+        }
+      }
+
+      $scope.hideApiKey = function() {
+        $scope.api_key = '';
+      }
+
 
   }]);
