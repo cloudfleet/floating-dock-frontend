3b9fc94988e2322573a877ac5562c87065a42809
Christoph Witzany <christoph@web.crofting.com>
2017-07-03 15:57:07 +0200

    improve search resultlist

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -19,4 +19,20 @@ angular.module('marinaFrontendApp')
           return $state.href('app.repository_list', {owner: search_result.name});
         }
       }
+      $scope.getName = function(search_result) {
+        if(search_result.owner_name) {
+          return search_result.owner_name + '/' + search_result.name;
+        } else {
+          return search_result.name;
+        }
+      }
+      $scope.getIconClass = function(search_result) {
+        if(search_result.owner_name) {
+          return "fa fa-cogs";
+        } else if(search_result.members) {
+          return "fa fa-users";
+        } else {
+          return "fa fa-user";
+        }
+      }
   }]);
